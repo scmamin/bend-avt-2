a4bce253e7e4de9770e3babee7ef470e260cec80
mamin <mamin@mytelemost.ru>
2025-05-26 13:57:38 +0300

    fix

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ const port = 3030;
 const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://127.0.0.1:27017/";
 const mongoClient = new MongoClient(url);
-count = 0;
+
+let count = 0; // объявляем count глобально
+
 async function run() {
 	try {
-		// Подключаемся к серверу
 		await mongoClient.connect();
-		// обращаемся к базе данных пользователя
 		const db = mongoClient.db(`test1`);
-		// выполняем пинг для проверки подключения
 		const result = await db.command({ ping: 1 });
 		console.log("Подключение с сервером успешно установлено");
+
 		const collection = db.collection("users");
 		count = await collection.countDocuments();
 		console.log(`В коллекции users ${count} документа/ов`);
@@ -26,22 +26,21 @@ async function run() {
 		console.log("Возникла ошибка");
 		console.log(err);
 	} finally {
-		// Закрываем подключение при завершении работы или при ошибке
-		3;
 		await mongoClient.close();
 		console.log("Подключение закрыто");
 	}
 }
+
+// Запускаем асинхронную функцию
 run().catch(console.error);
+
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
 	res.end(`Hello ${name}, you have ${count} documents`);
 });
+
 server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-const collection = db.collection("users");
-count = await collection.countDocuments();
-console.log(`В коллекции users ${count} документа/ов`);
+F
